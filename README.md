a07ec746d405efd7147cd2911cfd44d7e7d3abfb
SMM <sadafmmm@hotmail.com>
2018-02-08 06:32:16 +0000

    Added to GitHub

---
+++ README.md
@@ -0,0 +1,5 @@
+I can enter a URL and I will receive a shortened URL in the JSON response.
+
+If I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
+
+When I visit that shortened URL, it will redirect me to my original link.
---
+++ server.js
@@ -0,0 +1,90 @@
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
+
+const validUrl = require('valid-url');
+
+const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const app = express();
+
+var url = process.env.MONGOLAB_URI;
+var appURL = 'https://smm-url.glitch.me/';
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/', (req,res)=>{
+    var link = req.body.link;
+    var randID = Math.floor(Math.random()*90000) + 10000;
+    var linkData = {
+        original: link,
+        shortened: appURL+randID
+    };
+    if (validUrl.isUri(link)){
+        MongoClient.connect(url, (err,db)=>{
+            if (err) {
+                console.log('Unable to connect to the mongoDB server. Error:', err);
+            } else {
+                console.log('Connection established to', url);
+                const mydb = db.db('dwight').collection('urls');
+                
+                mydb.find({original:link}).toArray((err,result)=>{
+                    if(err) {console.log('Error: '+err)}
+                    if(result.length==0){
+                        console.log('Link not found in database');
+                        
+                        mydb.insertOne(linkData, (err,res)=>{
+                            if(err) throw err;
+                            console.log(JSON.stringify(linkData));
+                            //Close connection
+                            db.close();
+                        });
+                        res.json({
+                          original: link,
+                          shortened: appURL+randID
+                        });
+                    } else {
+                        console.log('Link already exists in database.');
+                        var orig = result[0].original;
+                        var short = result[0].shortened;
+                        res.json({
+                          original: orig,
+                          shortened: short
+                        });
+                        db.close();
+                    }
+                });
+            }
+        });
+    } else {
+        res.json({error: 'INVALID LINK! Use proper link formatting. Example: https://www.google.com'});
+    }
+});
+
+app.get('/:id', (req,res)=>{
+    var urlId = req.params.id;
+    MongoClient.connect(url, (err,db)=>{
+        console.log('Connected to database.');
+        if(err) throw err;
+        var mydb = db.db('dwight').collection('urls');
+        mydb.find({shortened: appURL+urlId}).toArray((err,result)=>{
+            if(err) throw err;
+            if(result.length==0){
+                res.json({error: 'INVALID LINK!'});
+                //Close database
+                db.close();
+            } else{
+                console.log('Redirecting...');
+                var redirectLink = result[0].original;
+                res.redirect(redirectLink);
+                db.close();
+            }
+        });
+    });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, ()=>{
+    console.log('Your app is listening on port: '+listener.address().port);
+})
